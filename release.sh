35d79f09228cbf44dfed24c977a3eb957f2de6d5
Miles Johnson <mileswjohnson@gmail.com>
2019-10-02 21:21:06 -0700

    Add release script.

---
+++ release.sh
@@ -0,0 +1,27 @@
+if [ "$1" == "" ]; then
+    echo "Version branch required as first argument."
+    exit
+fi
+
+if [ "$2" == "" ]; then
+    echo "Next version required as second argument."
+    exit
+fi
+
+# Checkout branch
+git checkout "$1"
+
+# Remove all deps
+rm -rf ./node_modules
+
+# Only install prod deps
+yarn install --production
+
+# Add modules to git
+git add -f ./node_modules
+
+# Tag new version
+git tag -fa "$1" -m "v$2"
+
+# Push changes
+git push origin "$1"
